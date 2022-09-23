56b576e4608580faa0bcea0c13ad3379d6adefbc
Matt Jewell <m@mattjewell.co.uk>
2022-09-23 09:45:28 +0200

    dark mode!

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,26 +1,38 @@
 import React from "react";
 import { globals } from "@/globals";
-import { Link, Text, VStack, HStack } from "@chakra-ui/react";
+import { HStack, Link, Text, useColorMode, VStack } from "@chakra-ui/react";
 
-export const Footer: React.FC = () => (
-  <VStack
-    as="footer"
-    width="full"
-    textAlign="center"
-    backgroundColor="gray.600"
-    bgGradient={"linear(to-r, blackAlpha.600, transparent)"}
-    color="white"
-    pt={8}
-    pb={4}
-  >
-    <Text as="p">{`© ${globals.yourName} ${new Date().getFullYear()}`}</Text>
-    <HStack>
-      <Link color="white" href="https://www.linkedin.com/in/mattjewell1/">
-        Connect on LinkedIn
-      </Link>
-      <Link color="white" href="/rss.xml">
-        Blog as RSS
-      </Link>
-    </HStack>
-  </VStack>
-);
+export function DarkMode() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <button onClick={() => toggleColorMode()}>
+      {colorMode === "dark" ? "☀️" : "🌙"}
+    </button>
+  );
+}
+
+export function Footer() {
+  return (
+    <VStack
+      as="footer"
+      width="full"
+      textAlign="center"
+      backgroundColor="gray.600"
+      bgGradient={"linear(to-r, blackAlpha.600, transparent)"}
+      color="white"
+      pt={8}
+      pb={4}
+    >
+      <DarkMode />
+      <Text as="p">{`© ${globals.yourName} ${new Date().getFullYear()}`}</Text>
+      <HStack>
+        <Link color="white" href="https://www.linkedin.com/in/mattjewell1/">
+          Connect on LinkedIn
+        </Link>
+        <Link color="white" href="/rss.xml">
+          Blog as RSS
+        </Link>
+      </HStack>
+    </VStack>
+  );
+}
